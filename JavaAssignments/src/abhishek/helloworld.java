3d1c65b853ef96fe2da37be13f490b5d21236402
Abhishek <abhisheks@nedbank.co.za>
2019-01-20 13:56:27 +0530

    adding hello wolrd file

---
+++ JavaAssignments/src/abhishek/helloworld.java
@@ -0,0 +1,14 @@
+package abhishek;
+
+public class helloworld {
+static void printName(String a) {
+		
+		System.out.println("Welcome"+" "+a);
+	}
+	
+
+	public static void main(String[] args) {
+		printName("Abhishek");
+	}
+
+}
