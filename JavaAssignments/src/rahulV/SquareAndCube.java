3bc32ed93f09df83373133d60d59f016910c03f8
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-20 22:55:52 +0530

    modified programme for Q3

--- JavaAssignments/src/rahulV/SquareAndCube.java
+++ JavaAssignments/src/rahulV/SquareAndCube.java
@@ -3,21 +3,21 @@ package rahulV;
 public class SquareAndCube {
 	
 
-	int square(int num) {
+	int square(int num) {  //method for square of number
 
 		return num * num;
 	}
 	
-	int cube(int num) {
+	int cube(int num) {    //method for cube of number
 
 		return num*num*num;
 	}
 
 
 	public static void main(String[] args) {
-		SquareAndCube s1 = new SquareAndCube();
-		System.out.println(s1.square(5));
-		System.out.println(s1.cube(8));
+		SquareAndCube s1 = new SquareAndCube();    
+		System.out.println(s1.square(5));  //25
+		System.out.println(s1.cube(8));    //512
 
 	}
 
