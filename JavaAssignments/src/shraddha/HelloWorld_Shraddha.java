68cf1b051c34f07e854a681727d3f6bb3007aae8
Shraddha.Sonawane <Shraddha.Sonawane@globant.com>
2019-01-20 14:47:03 +0530

    adding HellowWorld file and changes in Test file

---
+++ JavaAssignments/src/shraddha/HelloWorld_Shraddha.java
@@ -0,0 +1,12 @@
+package shraddha;
+
+public class HelloWorld_Shraddha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Shraddha");
+
+	}
+
+}
