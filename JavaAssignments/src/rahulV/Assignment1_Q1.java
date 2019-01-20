6adab64398bf986fabe6cfce321931ec8c08eef0
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-20 20:27:09 +0530

    Added assignment1 Q1

---
+++ JavaAssignments/src/rahulV/Assignment1_Q1.java
@@ -0,0 +1,45 @@
+package rahulV;
+
+public class Assignment1_Q1 {
+
+	int sum(int num1, int num2) {
+
+		// int sum = num1+num2;
+		return num1 + num2;
+	}
+
+	int sub(int num1, int num2) {
+
+		// int sub = num1-num2;
+		return num1 - num2;
+	}
+
+	int mul(int num1, int num2) {
+
+		// int mul = num1*num2;
+		return num1 * num2;
+	}
+
+	int div(int num1, int num2) {
+
+		//int div = num1 / num2;
+		return num1 / num2;
+	}
+
+	int rem(int num1, int num2) {
+
+		// int rem = num1%num2;
+		return num1 % num2;
+	}
+
+	public static void main(String[] args) {
+		Assignment1_Q1 s = new Assignment1_Q1();
+		System.out.println(s.sum(10, 20));
+		System.out.println(s.sub(30, 10));
+		System.out.println(s.mul(5, 10));
+		System.out.println(s.div(50, 10));
+		System.out.println(s.rem(41, 8));
+
+	}
+
+}
