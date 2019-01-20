e007202dbf92d066ac4e0e4bc10c8ea462798f96
Shital <am.ankushmahajan@gmail.com>
2019-01-20 23:55:25 +0530

    Assignment_Q6_Ankush

---
+++ JavaAssignments/src/ankush/TemperatureConverter.java
@@ -0,0 +1,14 @@
+package ankush;
+
+public class TemperatureConverter {
+	
+	static void temperature(float temperature)
+	{
+		System.out.println("Temperature Converted from Fahrenheit to Celsius : " +((temperature-32)*5)/9);
+	}
+	
+	public static void main(String[] args) {
+	float temperature=Float.parseFloat(args[0]);
+	TemperatureConverter.temperature(temperature);
+}
+}
