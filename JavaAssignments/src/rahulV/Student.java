cbe43e2a29e1452d83dc21e9a406bc27a0807164
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-21 20:45:25 +0530

    changes done

--- JavaAssignments/src/rahulV/Student.java
+++ JavaAssignments/src/rahulV/Student.java
@@ -9,10 +9,10 @@ public class Student {
 	public static void main(String[] args) {
 		Student s1 = new Student();
 		String name = args[0];
-		int rollNo = Integer.parseInt(args[1]);
+		int rollNo = Integer.parseInt(args[1]);     
 		int marks = Integer.parseInt(args[2]);
 		// s1.display("Rahul",1,100);
-		s1.display(name, rollNo, marks);
+		s1.display(name, rollNo, marks);    //provide user input from console
 		
 	}
 }
