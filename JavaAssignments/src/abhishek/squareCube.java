643a8ed6e71f8538d0fd2655b1a9de1643e94fd1
Abhishek <abhisheks@nedbank.co.za>
2019-01-21 11:29:39 +0530

    adding file for cube ans square program

---
+++ JavaAssignments/src/abhishek/squareCube.java
@@ -0,0 +1,20 @@
+package abhishek;
+
+public class squareCube {
+	static void square(int a) {
+
+		System.out.println(a * a);
+	}
+
+	static void cube(int a) {
+
+		System.out.println(a * a * a);
+	}
+
+	public static void main(String[] args) {
+
+		square(9);
+		cube(9);
+
+	}
+}
