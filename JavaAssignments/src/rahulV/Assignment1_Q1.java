eb0a70c83fdaec718041a06afa206ddab8f609ae
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-21 20:39:45 +0530

    changes done

--- JavaAssignments/src/rahulV/Assignment1_Q1.java
+++ JavaAssignments/src/rahulV/Assignment1_Q1.java
@@ -2,31 +2,31 @@ package rahulV;
 
 public class Assignment1_Q1 {  
 
-	int sum(int num1, int num2) {   //Addition
+	int sum(int num1, int num2) {   //method to calculate sum
 
 		// int sum = num1+num2;
 		return num1 + num2;
 	}
 
-	int sub(int num1, int num2) {   //substraction
+	int sub(int num1, int num2) {   //method to calculate substraction
 
 		// int sub = num1-num2;
 		return num1 - num2;
 	}
 
-	int mul(int num1, int num2) {   //multiplication
+	int mul(int num1, int num2) {   //method to calculate multiplication
 
 		// int mul = num1*num2;
 		return num1 * num2;
 	}
 
-	int div(int num1, int num2) {   //division
+	int div(int num1, int num2) {   //method to calculate division
 
 		//int div = num1 / num2;
 		return num1 / num2;
 	}
 
-	int rem(int num1, int num2) {   //reminder
+	int rem(int num1, int num2) {   //method to calculate reminder
 
 		// int rem = num1%num2;
 		return num1 % num2;
