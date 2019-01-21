535d8f74b8f9c8ebdfde15b05253d01701710422
unknown <vishalkhatri.13@gmail.com>
2019-01-21 23:19:09 +0530

    correct formatting

--- JavaAssignments/src/vishal/SquareCube.java
+++ JavaAssignments/src/vishal/SquareCube.java
@@ -1,26 +1,22 @@
 package vishal;
 
-public class SquareCube 
-{
-	public int square(int num)
-	{
-		int result=num*num;
+public class SquareCube {
+	public int square(int num) {
+		int result = num * num;
 		return result;
 	}
 
-	public int cube(int num)
-	{
-		int result=num*num*num;
+	public int cube(int num) {
+		int result = num * num * num;
 		return result;
 	}
-	
-	public static void main(String[]args)
-	{
-		SquareCube sc=new SquareCube();
-		int square=sc.square(5);
-		int cube=sc.cube(5);
-		
-		System.out.println("Square of a num is "+square);
-		System.out.println("Cube of a num is "+cube);
+
+	public static void main(String[] args) {
+		SquareCube sc = new SquareCube();
+		int square = sc.square(5);
+		int cube = sc.cube(5);
+
+		System.out.println("Square of a num is " + square);
+		System.out.println("Cube of a num is " + cube);
 	}
 }
