ca7da5acfc00e191d458705462801e4865f5a095
unknown <vishalkhatri.13@gmail.com>
2019-01-22 00:26:16 +0530

    Fah to Cel Q6 Vishal

---
+++ JavaAssignments/src/vishal/ConvertTemp.java
@@ -0,0 +1,15 @@
+package vishal;
+
+public class ConvertTemp {
+	public float convertTemp(float fahTemp) {
+		float cel = (fahTemp - 32) * 5 / 9;
+		return cel;
+	}
+
+	public static void main(String[] args) {
+		ConvertTemp ct = new ConvertTemp();
+		float convertedTemp = ct.convertTemp(12.5f);
+		System.out.println("Converted Tempture in Celsius is " + convertedTemp);
+
+	}
+}
