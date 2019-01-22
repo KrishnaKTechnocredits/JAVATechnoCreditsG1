1748718790d69dd2b74371a48370017a7c71cb47
mayur <mayursuryawanshi777@gmail.com>
2019-01-22 15:29:42 +0530

    Added Reminder functionality

--- JavaAssignments/src/mayur/Calculator.java
+++ JavaAssignments/src/mayur/Calculator.java
@@ -2,21 +2,36 @@ package mayur;
 
 public class Calculator
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
 		Calculator c = new Calculator();
-		System.out.println("Numbers Enter by user are : " + args[0] + "," + args[1]);
-		System.out.println("Addition is  " + c.addition(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
-		System.out.println("Substraction is  " + c.substraction(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
-		System.out.println("multiplication is  " + c.multiplication(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
-		System.out.println("division is  " + c.division(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
+		double val1 = Double.parseDouble(args[0]);
+		double val2 = Double.parseDouble(args[1]);
+		System.out.println("Numbers Enter by user are : " + val1 + "," + val2);
+		System.out.println("Addition is  " + c.addition(val1, val2));
+		System.out.println("Substraction is  " + c.substraction(val1, val2));
+
+		if (val1 == 0 || val2 == 0) 
+		{
+			System.out.println("You cannot multiply or divide number by Zero(0)");
+		} 
+		else 
+		{
+			System.out.println("Multiplication is  " + c.multiplication(val1, val2));
+			System.out.println("Division is  " + c.division(val1, val2));
+			System.out.println("Remainder is  " + c.remainder(val1, val2));
+
+		}
+
 	}
+
 	public double addition(double value1, double value2) 
 	{
 		return value1 + value2;
 	}
 
 	public double substraction(double value1, double value2) 
+	
 	{
 		return value1 - value2;
 	}
@@ -29,5 +44,10 @@ public class Calculator
 	public double division(double value1, double value2) 
 	{
 		return value1 / value2;
+	}
+
+	public double remainder(double value1, double value2) 
+	{
+		return value1 % value2;
 	}
 }
