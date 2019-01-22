1789ce8e39726c22452dc0ea65dbae95625a3eff
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-22 20:17:49 +0530

    changes done to pass float value

--- JavaAssignments/src/rahulV/Temperature.java
+++ JavaAssignments/src/rahulV/Temperature.java
@@ -3,8 +3,8 @@ package rahulV;
 public class Temperature {
 
 	float celsius;
-
-	void convert(int fahrenheit) { // method to convert fahrenheit into celsius
+//code changed to pass float value
+	void convert(float fahrenheit) { // method to convert fahrenheit into celsius
 
 		celsius = (fahrenheit - 32);
 		float var = celsius * 5 / 9;
@@ -15,7 +15,7 @@ public class Temperature {
 
 	public static void main(String[] args) {
 		Temperature t = new Temperature();
-		t.convert(60);
+		t.convert(60.5f);
 
 	}
 
