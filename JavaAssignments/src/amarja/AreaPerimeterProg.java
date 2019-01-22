0b8745c1b2288a5d03f679d7f0afebcc7a1cfeb3
AmarjaBTechnocredits <amarja.belorkar@gmail.com>
2019-01-22 20:49:12 +0530

    Assignment_Q4_Amarja

---
+++ JavaAssignments/src/amarja/AreaPerimeterProg.java
@@ -0,0 +1,31 @@
+package amarja;
+//Program to find area and Perimeter of Rectangle and Circle
+public class AreaPerimeterProg {
+	public static void main(String[] args) {
+		AreaPerimeterProg a =new AreaPerimeterProg();
+		//Arguments to be passed as unit,length and breadth for Rectangle
+		//Arguments to be passed as unit,radius for circle
+		a.CalRectareaperimtr(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+		a.CalCircareaperimtr(args[3],Float.parseFloat(args[4]));
+		}
+	void CalRectareaperimtr(String unit,int length,int breadth)
+	{
+		int area;int perimtr;
+		area=length*breadth;
+		perimtr=2*(length+breadth);
+		System.out.println("Length of Rectangle = " + length + unit);
+		System.out.println("Breadth of Rectangle = " + breadth+unit );
+		System.out.println("Area of Rectangle =" + area + "sq"+unit);
+		System.out.println("Perimeter of Rectangle =" + perimtr +unit);
+	}
+	void CalCircareaperimtr(String unit,float radius)
+	{
+		double area;double perimtr;
+		area=3.14*radius*radius;
+		perimtr=2*3.14*radius;
+		System.out.println("Radius of Circle = " + radius + unit);
+		System.out.println("Area of Circle =" + area + "sq"+unit);
+		System.out.println("Perimeter of Circle=" + perimtr +unit);	
+	}
+}
+
