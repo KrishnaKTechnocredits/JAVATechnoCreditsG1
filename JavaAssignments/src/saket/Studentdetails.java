2e38e1d70acb0f1049ff75d25a7d35c60d666411
unknown <saketnagmote@gmail.com>
2019-01-23 22:22:53 +0530

    Adding assignment 2

---
+++ JavaAssignments/src/saket/Studentdetails.java
@@ -0,0 +1,17 @@
+package saket;
+
+public class Studentdetails {
+
+	public static void main(String[] args) {
+
+		System.out.println("Roollnum is " + args[0]);
+
+	    System.out.println("Name of the Student is " + args[1]);
+
+		int i = Integer.parseInt(args[2]);
+		System.out.println("marks of the student " + args[1] + " is " + i);
+		
+		
+	}
+
+}
