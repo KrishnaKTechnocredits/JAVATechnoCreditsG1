2473f6990183f9bdc1b956d028ea822c77277d0b
mayur <mayursuryawanshi777@gmail.com>
2019-01-23 22:38:56 +0530

    Done Formatting

--- JavaAssignments/src/mayur/SwapNumbers.java
+++ JavaAssignments/src/mayur/SwapNumbers.java
@@ -2,24 +2,20 @@ package mayur;
 
 public class SwapNumbers {
 
-	public static void main(String args[]) 
-	{
-		float Num1=(float) Double.parseDouble(args[0]);
-		float Num2=(float) Double.parseDouble(args[1]);
+	public static void main(String args[]) {
+		float Num1 = (float) Double.parseDouble(args[0]);
+		float Num2 = (float) Double.parseDouble(args[1]);
 		SwapNumbers s = new SwapNumbers();
-		System.out.println("Numbers before swaping: Number1 ="+Num1+" and Number2 ="+Num2);
+		System.out.println("Numbers before swaping: Number1 =" + Num1 + " and Number2 =" + Num2);
+		s.swap(Num1, Num2);
 
-		s.swap(Num1,Num2);
-		
 	}
 
-	void swap(float a,float b)
-	{
+	void swap(float a, float b) {
 		float temp;
-		temp =a;
-		a= b;
-		b= temp;
-		
-		System.out.println("Numbers after swaping: Number1 ="+a+" and Number2 ="+b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("Numbers after swaping: Number1 =" + a + " and Number2 =" + b);
 	}
 }
