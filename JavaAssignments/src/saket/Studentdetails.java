06da1ec3df44c3ac72e2e9b07c01eb744c0c83d7
unknown <saketnagmote@gmail.com>
2019-01-25 14:40:48 +0530

    adding the changes

--- JavaAssignments/src/saket/Studentdetails.java
+++ JavaAssignments/src/saket/Studentdetails.java
@@ -1,17 +1,42 @@
 package saket;
 
 public class Studentdetails {
+	
+	int rollnum;
+	String name;
+	int marks;
+	
 
 	public static void main(String[] args) {
-
-		System.out.println("Roollnum is " + args[0]);
-
-	    System.out.println("Name of the Student is " + args[1]);
-
-		int i = Integer.parseInt(args[2]);
-		System.out.println("marks of the student " + args[1] + " is " + i);
 		
+		
+		Studentdetails A=new Studentdetails();
+		
+		int a=Integer.parseInt(args[0]);
+		String b=args[1];
+		int c=Integer.parseInt(args[2]);
+		
+		A.setdata(a,b,c);	
+		A.display(a,b,c);
 		
 	}
+		
+	void setdata(int a,String b,int c)
+	{
+		
+		rollnum=a;
+		name=b;
+		marks=c;
+		
+	}	 
+		
+		
+void display(int a,String b,int c )
+	{
+	System.out.println("Roollnum is " + rollnum );
+    System.out.println("Name of the Student is " + name );
+	System.out.println("marks of the student " + name + " is " + marks);
+
+	} 
 
 }
