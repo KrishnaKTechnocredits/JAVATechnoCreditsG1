c004ff39702b81524f2d46cb9c8a5fa3a6a783c4
unknown <saketnagmote@gmail.com>
2019-01-25 16:57:48 +0530

    done

--- JavaAssignments/src/saket/swapwiththirdvaiable.java
+++ JavaAssignments/src/saket/swapwiththirdvaiable.java
@@ -2,23 +2,22 @@ package saket;
 
 public class swapwiththirdvaiable {
 
-	int a=10;
-	int b=20;
-	int c;
-	
-	void swap()
-	{
-	c=a;
-	a=b;
-    b=c;	
-	System.out.println("a="+a);
-	System.out.println("b="+b);
-    };
-	    public static void main(String[] args) {
+	int swap(int a, int b) {
+		int c;
+		c = a;
+		a = b;
+		b = c;
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+
+		return 0;
+	};
+
+	public static void main(String[] args) {
 		swapwiththirdvaiable A = new swapwiththirdvaiable();
-		A.swap();
-		
+		A.swap(5, 10);
+
 		// TODO Auto-generated method stub
-		}
+	}
 
 }
