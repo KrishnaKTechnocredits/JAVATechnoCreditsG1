53a9968f9a76e07419a698db14dfd1fe36acd634
RahulVTechnocredits <rahulvarma411@gmail.com>
2019-01-26 21:34:00 +0530

    changes done to take input from user

--- JavaAssignments/src/rahulV/TimeConversion.java
+++ JavaAssignments/src/rahulV/TimeConversion.java
@@ -5,7 +5,7 @@ public class TimeConversion {
 	static int hours;
 	static int minutes;
 
-	void Time(int seconds) {
+	void time(int seconds) {
 
 		hours = seconds / 3600;
 		int s1 = seconds % 3600;
@@ -14,10 +14,11 @@ public class TimeConversion {
 		System.out.println("Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] a) {
 
 		TimeConversion t = new TimeConversion();
-		t.Time(30000);
+		int seconds = Integer.parseInt(a[0]);
+		t.time(seconds);
 
 	}
 
