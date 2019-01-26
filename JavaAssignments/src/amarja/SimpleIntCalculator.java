1cd5bab0f17b213721044119249853200c34bb6b
AmarjaBTechnocredits <amarja.belorkar@gmail.com>
2019-01-26 22:06:15 +0530

    Modified the method name to begin with small and added return statement as per previous comments

--- JavaAssignments/src/amarja/SimpleIntCalculator.java
+++ JavaAssignments/src/amarja/SimpleIntCalculator.java
@@ -6,14 +6,14 @@ public class SimpleIntCalculator {
 		 Double Priamount=Double.parseDouble(args[0]);
 		 int time=Integer.parseInt(args[1]);
 		 float rate=Float.parseFloat(args[2]);
-		 a.CalSimpleInterest(Priamount,time,rate);
+		 System.out.println("Simple Interest = " + a.calSimpleInterest(Priamount,time,rate));
 	}
-	void CalSimpleInterest(Double priamount, int time, float rate) 
+	Double calSimpleInterest(Double priamount, int time, float rate) 
 	{
 		Double simint=(priamount*time*rate)/100;
 		System.out.println("Principle amount= " + priamount);
 		System.out.println("Time= " + time);
 		System.out.println("Rate= " + rate);
-		System.out.println("Simple Interest = "+simint);	
+		return simint;
 	}
 }
