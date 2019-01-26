80e4492b07cbfaf3b824c5f7abbdaaee0a38b8bc
AmarjaBTechnocredits <amarja.belorkar@gmail.com>
2019-01-26 21:36:56 +0530

    Removed unused variable from proram q3

--- JavaAssignments/src/amarja/SquareCubeprog.java
+++ JavaAssignments/src/amarja/SquareCubeprog.java
@@ -1,21 +1,15 @@
 package amarja;
 //WAP to find square and cube of a number.
 public class SquareCubeprog {
-	static int square;
-	static int cube;
 	public static void main(String [] args){
 		SquareCubeprog a =new SquareCubeprog();
 		System.out.println("Square of Number "+(Integer.parseInt(args[0]))+" = " + a.findsquare(Integer.parseInt(args[0])));
 		System.out.println("Cube of Number "+(Integer.parseInt(args[0]))+" = " + a.findcube(Integer.parseInt(args[0])));
 	}
 	int findsquare(int num){
-		int square;
-		square=num*num;
-		return square;
+		return num*num;
 		}
 	int findcube(int num){
-		int cube;
-		cube=num*num*num;
-		return cube;
+		return num*num*num;
 		}
 }
