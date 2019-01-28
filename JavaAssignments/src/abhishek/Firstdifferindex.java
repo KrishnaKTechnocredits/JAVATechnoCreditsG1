fb85047b4eb9361578309e25e428b1301aa02ba7
Abhishek <abhisheks@nedbank.co.za>
2019-01-29 00:16:27 +0530

    adding file for array assignment

---
+++ JavaAssignments/src/abhishek/Firstdifferindex.java
@@ -0,0 +1,23 @@
+package abhishek;
+
+public class Firstdifferindex {
+	
+	public static void main(String[] args) {
+		int [] arr1 = {10,2,18,14,3};
+	    int [] arr2 = {10,2,18,1,3};
+		for(int index = 0;index< arr1.length;index++) {
+		if(arr1[index]!=arr2[index]) {
+				
+				System.out.println("Values are not matching at index -> "+index);
+				break;
+			}
+			
+		}
+			
+			
+			
+			
+		}
+	}
+
+
