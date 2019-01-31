0acf528b011e3ce5d9958c5f20f3d5a3c69c8ec3
FarhinSTechnocredits <farhin003@gmail.com>
2019-02-01 00:36:07 +0530

    PR contains Q7 and Q8

---
+++ JavaAssignments/src/farhin/FindDifferIndex.java
@@ -0,0 +1,48 @@
+package farhin;
+
+import java.util.Scanner;
+
+public class FindDifferIndex {
+	public static void main(String[] args) {
+		FindDifferIndex findIndex = new FindDifferIndex();
+
+		Scanner sc = new Scanner(System.in);
+		int[] firstArr = findIndex.takeInputs( "first", sc);
+		int[] secondArr = findIndex.takeInputs("second", sc);
+		sc.close();
+		if (firstArr.length != secondArr.length) {    //checking array length same or not
+			System.out.println("Array lengths do not match");
+			return;
+		}
+		int differIndex = findIndex.findDifferIndex(firstArr, secondArr);
+		if(differIndex<0) {                           // check differIndex value after to validate array maching
+			System.out.println("Arrays are matching");
+		}else {
+
+			System.out.println("Values are not matching at index-> " + differIndex);	
+		}
+	}
+
+	int[] takeInputs(String str, Scanner sc) {
+		System.out.println("Enter "+str+" Array Size");
+		int arrSize = sc.nextInt();
+		int[] arrNum = new int[arrSize];
+		System.out.println("Enter "+str+" Array ");
+		for (int index = 0; index < arrSize; index++) {
+			arrNum[index] = sc.nextInt();
+		}
+		return arrNum;
+	}
+
+	int findDifferIndex(int[] arr1, int[] arr2) {
+		int differIndex = -1;
+		for (int index = 0; index < arr1.length; index++) {  //iterate through both array 
+			if (arr1[index] != arr2[index]) {    //check both array's index value
+				differIndex = index;
+				break;
+			}
+		}
+		return differIndex;
+
+	}
+}
