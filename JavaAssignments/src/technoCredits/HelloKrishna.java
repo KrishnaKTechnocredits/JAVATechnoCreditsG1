377d156bd9afe8c9dcdf7fb14c25f6d398fd3496
Krishna_Kanani <krishna.kanani@hp.com>
2019-02-03 11:21:15 +0530

    added

--- JavaAssignments/src/technoCredits/HelloKrishna.java
+++ JavaAssignments/src/technoCredits/HelloKrishna.java
@@ -2,6 +2,8 @@ package technoCredits;
 
 public class HelloKrishna {
 
+	
+	// Hi added a comment
 	public static void main(String[] args) {
 		System.out.println("Hello Krishna !!");
 	}
---
+++ JavaAssignments/src/technoCredits/TestCheck1.java
@@ -0,0 +1,9 @@
+package technoCredits;
+
+public class TestCheck1 {
+
+	public static void main(String[] args) {
+		System.out.println("In test check class");
+	}
+	
+}
