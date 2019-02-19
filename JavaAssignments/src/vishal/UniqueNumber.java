41263d147be69ca838ac82d4e5cd9cdbc43cb2c2
unknown <vishalkhatri.13@gmail.com>
2019-02-19 20:22:53 +0530

    Update Unique Number Prog

--- JavaAssignments/src/vishal/UniqueNumber.java
+++ JavaAssignments/src/vishal/UniqueNumber.java
@@ -1,36 +1,53 @@
 package vishal;
 
 public class UniqueNumber {
+	static int count = 0;
 
-	public static void main(String[] args) 
-	{
-     int[] arr1={1,2,3,4,2,5};
-     int[] arr2={5,6,7,8,9,7};
-     int length=arr1.length+arr2.length;
-     int[] uniqueArray=new int[length];
-     
-     for(int index1=0;index1<arr1.length;index1++)
-     {
-    	 for(int index2=1;index2<arr1.length;index2++)
-    	 {
-    		 if(arr1[index1]==arr1[index2])
-    		 {
-    			 index2=index2+1;
-    			 break;
-    		 }
-    		 else
-    		 {
-    			 uniqueArray[index1]=arr1[index1];
-              break;    		
-             }
-    	 }
-     }
-     
-     for(int i=0;i<length;i++)
-     {
-     System.out.println(uniqueArray[i]);
-     }
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 2, 3, 4, 2, 5 };
+		int[] arr2 = { 5, 6, 7, 8, 9, 7 };
+		int length = arr1.length + arr2.length;
+		int[] uniqueArray = new int[length];
+
+
+		for (int i = 0; i < arr1.length; i++) {
+			setElement(uniqueArray, arr1[i]);
+
+		}
 		
+		for (int j = 0; j < arr2.length; j++) {
+			setElement(uniqueArray, arr2[j]);
+
+		}
+
+		displayIntArray(uniqueArray);
+
 	}
 
+	static void setElement(int[] uniqueArray, int temp) {
+		boolean state = false;
+
+		for (int j = 0; j < uniqueArray.length; j++) {
+
+			if (uniqueArray[j] == temp) {
+				state = true;
+				break;
+			}
+		}
+			if (state == false) {
+				uniqueArray[count] = temp;
+				count = count+1;
+			}}
+		
+	
+
+	static void displayIntArray(int[] uniqueArray) {
+
+		for (int i = 0; i < uniqueArray.length; i++) {
+
+			if (uniqueArray[i] != 0)
+				System.out.println(uniqueArray[i]);
+		}
+
+	}
 }
