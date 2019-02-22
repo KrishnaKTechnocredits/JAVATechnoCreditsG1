494a9a2527ec2ec4e389568cc8bb9610bbede609
Shraddha.Sonawane <Shraddha.Sonawane@globant.com>
2019-02-22 14:23:38 +0530

    exception assignment/question 5

--- JavaAssignments/src/shraddha/ExceptionAssg4.java
+++ JavaAssignments/src/shraddha/ExceptionAssg4.java
@@ -5,11 +5,11 @@ a. m2() called from m1().
 *Unchecked Exception occurred [lets say ArrayIndexOutOfBoundsException], methods throws an
 exception and same method handle that exception using try catch. what you think exception
 propagated to m1() ? m1() method has to handle that exception ?
-----> Ans: not a compulsion to handel it in m1. it internally gets propagated.
+----> Ans: not a compulsion to handle it in m1. it internally gets propagated.
 
 *Checked Exception occurred [lets say IOException], methods throws that checked exception and
 same method handle that exception using try catch. what you think exception propagated to m1() ?
-m1() method has to handle that exception ? -----> yes m1 has to handel the exception. and cannot handel it in m2.
+m1() method has to handle that exception ? -----> yes m1 has to handle the exception. and cannot handle it in m2.
  * 
  ------------------------------------------------------------------------------*/
 
